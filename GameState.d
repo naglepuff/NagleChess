86ec074282216bbb980b1ed1601609e62d961c36
mike <m.nagler995@gmail.com>
2021-03-22 17:34:19 -0400

    remove files no longer needed

--- GameState.d
+++
@@ -1,2 +0,0 @@
-obj/GameState.o: src/GameState.cpp src/include/game_state.h \
- src/include/./player_state.h src/include/././starting_values.h
--- main.d
+++
@@ -1,2 +0,0 @@
-obj/main.o: src/main.cpp src/include/game_state.h \
- src/include/./player_state.h src/include/././starting_values.h
