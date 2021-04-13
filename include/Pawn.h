b08daaa2553ddd0a9b0efc55afefcbea281237d7
mike <m.nagler995@gmail.com>
2021-04-13 16:24:34 -0400

    fleshed out pawn generation

--- include/Pawn.h
+++ include/Pawn.h
@@ -3,9 +3,14 @@
 class Pawn {
 
     public:
-    static std::vector<Move> GeneratePawnMoves(GameState state);
+    const static PieceType Type = PieceType::Pawn;
+    const static std::vector<int> CAPTURE_DIRS_B;
+    const static std::vector<int> CAPTURE_DIRS_W;
+    static std::vector<Move> GeneratePawnMoves(GameState& state);
     //private:
-    static uint64_t PotentialPawnMoveSquares(uint64_t position, PlayerColor color);
+    static uint64_t PotentialPawnMoveSquares(uint64_t position, PlayerColor color);    // could be useful if we could generate some "Position" object...
     static uint64_t PotentialPawnAttackSquares(uint64_t position, PlayerColor color);
-    static std::vector<Move> GenerateSinglePawnMoves(GameState state, uint64_t onePawn, PlayerState activePlayer, PlayerState inactivePlayer); 
+    static std::vector<Move> GenerateSinglePawnMoves(GameState& state, uint64_t onePawn, PlayerState& activePlayer, PlayerState& inactivePlayer); 
 };
+const std::vector<int> Pawn::CAPTURE_DIRS_B ({SOUTHEAST, SOUTHWEST});
+const std::vector<int> Pawn::CAPTURE_DIRS_W ({NORTHEAST, NORTHWEST});
--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-vector<Move> Pawn::GeneratePawnMoves(GameState state) {
+vector<Move> Pawn::GeneratePawnMoves(GameState& state) {
 
     PlayerState activePlayer = state.white;
     PlayerState inactivePlayer = state.black;
@@ -26,29 +26,54 @@ vector<Move> Pawn::GeneratePawnMoves(GameState state) {
     return pawnMoves;
 }
 
-vector<Move> Pawn::GenerateSinglePawnMoves(GameState state, uint64_t onePawn, PlayerState activePlayer, PlayerState inactivePlayer) {
+vector<Move> Pawn::GenerateSinglePawnMoves(GameState& state, uint64_t onePawn, PlayerState& activePlayer, PlayerState& inactivePlayer) {
 
     // for each piece we have to do some stuff to get things like occupied squares... there's probably a better way to do this, with like a struct or something
     // to memoize this part
+    vector<Move> singlePawnMoves;
     uint64_t opponentSquares = inactivePlayer.GetOccupiedSquares();
     uint64_t ownSquares = activePlayer.GetOccupiedSquares();
     uint64_t occupiedSquares = opponentSquares | ownSquares;
     int moveDirection = (activePlayer.color == PlayerColor::White) ? NORTH : SOUTH;
-
-    // generate non-capture pawn moves
     uint64_t startingPosition = (activePlayer.color == PlayerColor::White) ? WHITE_PAWNS : BLACK_PAWNS;
     int maxDistance = (onePawn & startingPosition > 0) ? 2 : 1;
+    
+    // generate non-capture pawn moves
     int numSquaresMoved = 0;
     uint64_t currentSquare = onePawn; // copy onePawn into a starting position of sorts
-    uint64_t nextSquare = NextSquare(onePawn, moveDirection);
-    while(numSquaresMoved < maxDistance && !(nextSquare & opponentSquares)) {
+    uint64_t nextSquare = NextSquare(currentSquare, moveDirection);
+    while(numSquaresMoved < maxDistance && !(nextSquare & occupiedSquares)) {
 
-        // add the move to the  list 
+        // add the move to the  list
+        Move move = Move(Pawn::Type, activePlayer.color, onePawn, nextSquare, false);
+        singlePawnMoves.push_back(move);
+        
+        currentSquare = nextSquare;
+        nextSquare = NextSquare(currentSquare, moveDirection);
+        numSquaresMoved++;
     }
 
-    // generate captureing pawn moves
+    // generate capturing pawn moves
+    const vector<int> dirs = (moveDirection == NORTH) ? Pawn::CAPTURE_DIRS_W : Pawn::CAPTURE_DIRS_B;
+    for(int dir : dirs) {
 
-    return vector<Move>();
+        nextSquare = NextSquare(onePawn, dir);
+        if(nextSquare > 0) {
+
+            if((nextSquare & opponentSquares)) {
+
+                Move move = Move(Pawn::Type, activePlayer.color, onePawn, nextSquare, true);
+                singlePawnMoves.push_back(move);
+            } else if(nextSquare == state.enPassantTarget) {
+
+                Move move = Move(MoveType::EnPassantCapture, activePlayer.color, onePawn, nextSquare);
+                singlePawnMoves.push_back(move);
+            }
+        }
+    }
+
+
+    return singlePawnMoves;
 }
 
 
