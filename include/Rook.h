5067670e01683e3c8d62ff1e283080a1584e803b
mike <m.nagler995@gmail.com>
2021-04-19 14:38:17 -0400

    wrote first draft of rook move generation

---
+++ include/Rook.h
@@ -0,0 +1,12 @@
+#include "./GameState.h"
+
+class Rook {
+
+    public:
+    const static PieceType Type = PieceType::Rook;
+    const static std::vector<int> DIRS;
+    static std::vector<Move> GenerateRookMoves(GameState& state);
+
+    static std::vector<Move> GenerateSingleRookMoves(GameState& state, uint64_t oneRook, PlayerState& activePlayer, PlayerState& inactivePlayer);
+
+};
---
+++ src/Rook.cpp
@@ -0,0 +1,66 @@
+/** 
+ * Move generation code 4 rooks based on given game state. 
+ * Rooks can move in the 4 cardinal directions.
+ * In this engine, rooks are moved by KINGS when they castle. 
+ * This module does not care about castling.
+**/
+
+#include "../include/Rook.h"
+
+using namespace std;
+
+const std::vector<int> Rook::DIRS ({NORTH, EAST, SOUTH, WEST});
+
+vector<Move> Rook::GenerateRookMoves(GameState& state) {
+
+    PlayerState activePlayer = state.white;
+    PlayerState inactivePlayer = state.black;
+    if(state.activeColor == PlayerColor::Black) {
+        activePlayer = state.black;
+        inactivePlayer = state.white;
+    }
+
+    vector<Move> rookMoves;
+
+    for(int i = 0; i < 64; i++) {
+
+        uint64_t currentSquare = (uint64_t) 1 << i;
+
+        if((activePlayer.rooks & currentSquare) > 1) {
+            vector<Move> singleRookMoves = GenerateSingleRookMoves(state, currentSquare, activePlayer, inactivePlayer);
+            rookMoves.insert(rookMoves.end(), singleRookMoves.begin(), singleRookMoves.end());
+        }
+    }
+    return rookMoves;
+}
+
+vector<Move> Rook::GenerateSingleRookMoves(GameState& state, uint64_t oneRook, PlayerState& activePlayer, PlayerState& inactivePlayer) {
+
+    vector<Move> singleRookMoves;
+    uint64_t opponentSquares = inactivePlayer.GetOccupiedSquares();
+    uint64_t ownSquares = activePlayer.GetOccupiedSquares();
+    uint64_t occupiedSquares = ownSquares | opponentSquares;
+
+    for(int dir: DIRS) {
+        
+        uint64_t currentSquare = oneRook;
+        uint64_t nextSquare = NextSquare(currentSquare, dir);
+
+        while(nextSquare > 0 && !(nextSquare & occupiedSquares)) {
+
+            // in here are reversible moves.
+            Move move = Move(PieceType::Rook, activePlayer.color, oneRook, nextSquare, false);
+            if(state.IsLegal(move)) { singleRookMoves.push_back(move);}
+
+            currentSquare = nextSquare;
+            nextSquare = NextSquare(currentSquare, dir);
+        }
+
+        // next square might have an enemy piece that we can capture!
+        if((nextSquare & opponentSquares)) {
+            Move move = Move(PieceType::Rook, activePlayer.color, oneRook, nextSquare, true);
+            if(state.IsLegal(move)){singleRookMoves.push_back(move);}
+        }
+    }
+    return singleRookMoves;
+}
